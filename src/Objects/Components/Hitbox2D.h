3f245d28cb608fd91f2bfa6fd6dc1448bdeb8522
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-06-01 12:02:13 -0700

    Update Hitbox2D.h

--- src/Objects/Components/Hitbox2D.h
+++ src/Objects/Components/Hitbox2D.h
@@ -1,1 +1,12 @@
-
+#define HITBOX2D_H
+class HitBox2D{
+  Vector2 Position;
+  Vector2 Size;
+  bool HitboxCollision;
+  bool MouseCollision;
+  bool Collide;
+  HitBox2D(Vector2 position, Vector2 Size);
+  HitBox2D();
+  void Update();
+}
+#endif
